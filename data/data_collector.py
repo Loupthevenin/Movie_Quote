a6192229e2a8a8e2e5fb0f0c8f8f56b0678d3226
MrShadowsWolf <loup.thevenin.1@gmail.com>
2023-09-24 11:45:04 +0200

    Scrap data movie in json

---
+++ data/data_collector.py
@@ -0,0 +1,78 @@
+from functions import *
+import json
+import os
+
+
+path = os.path.join(os.path.dirname(__file__), 'data_movies.json')
+data_movies = {"title_movies": [],
+               "type_movie": [],
+               "id_movies": [],
+               "director_movies": [],
+               "release_movies": [],
+               "number_quotes": []
+               }
+
+
+state_number = 3300
+i = 1
+
+
+if not os.path.exists(path):
+    with open(path, 'w') as file_json:
+        json.dump({}, file_json)
+
+
+def update_database(data):
+    title_all = soups.find('h1').text
+    if "Erreur 410" in title_all:
+        print("page non trouvé")
+    elif "Erreur 404" in title_all:
+        print("page non trouvé")
+    else:
+        if "Film" in title_all:
+            data['title_movies'].append(title_all.replace('Film - ', ""))
+            data['type_movie'].append("Film")
+        elif "Série" in title_all:
+            data['title_movies'].append(title_all.replace('Série - ', ""))
+            data['type_movie'].append("Série")
+        data['id_movies'].append(i)
+        data['director_movies'].append(find_tag_dd(soups, 2))
+        data['release_movies'].append(find_tag_dd(soups, 3))
+        data['number_quotes'].append(find_tag_dd(soups, 4))
+
+
+with open(path, 'r', encoding="utf-8") as f:
+    db = json.load(f)
+
+
+if data_movies.keys() == db.keys() and db['id_movies']:
+    encode = 1
+    i = db['id_movies'][-1] + 1
+    while not i > state_number:
+        print(i)
+        soups = html_soup(url_movie, i, 1)
+        if soups != "ERROR":
+            update_database(db)
+            i += 1
+        elif soups == "ERROR":
+            i += 1
+            continue
+else:
+    encode = 2
+    while not i > state_number:
+        print(i)
+        soups = html_soup(url_movie, i, 1)
+        if soups != "ERROR":
+            update_database(data_movies)
+            i += 1
+        elif soups == "ERROR":
+            i += 1
+            continue
+
+
+if encode == 1:
+    with open(path, 'w', encoding="utf-8") as f:
+        json.dump(db, f, ensure_ascii=False, indent=4)
+elif encode == 2:
+    with open(path, 'w', encoding="utf-8") as f:
+        json.dump(data_movies, f, ensure_ascii=False, indent=4)
--- functions.py
+++ functions.py
@@ -9,19 +9,43 @@ quote_list = []  # liste de toutes les citations
 character_list = []  # liste de tout les personnages de chaque citation
 
 
-def find_number_pages(id_number) -> int:
-    html_find = requests.get(f"{url_movie}{id_number}")
-    soup_find = BeautifulSoup(html_find.text, 'html.parser')
-    dl_tag = soup_find.find('dl')
+def html_soup(url, ids, pages):
+    if ids and pages:
+        html_pages = requests.get(f"{url}{ids}-{pages}")
+        if html_pages.status_code != 404 or html_pages.status_code != 410:
+            soup = BeautifulSoup(html_pages.text, 'html.parser')
+            return soup
+        else:
+            return "ERROR"
+    elif not ids:
+        html = requests.get(url)
+        if html.status_code != 404 or html.status_code != 410:
+            soup = BeautifulSoup(html.text, 'html.parser')
+            return soup
+        else:
+            return "ERROR"
+
+
+def find_tag_dd(soup, number_of_dd):
+    dl_tag = soup.find('dl')
     if dl_tag:
         dd_tags = dl_tag.find_all('dd')
-        if len(dd_tags) >= 4:
-            number_quote = int(dd_tags[3].get_text())
-            return find_number_boucle(analyse_number=number_quote)
+        if len(dd_tags) >= number_of_dd:
+            result = dd_tags[number_of_dd - 1].get_text()
+            if result.isdigit():
+                return int(result)
+            else:
+                return str(result)
         else:
             print("Il n'y pas assez de balises <dd> à l'interieur de la balise <dl>.")  # a modifier
     else:
         print("La balise <dl> n'a pas été trouvée.")  # a modifier
+
+
+def find_number_pages(id_number) -> int:
+    soups = html_soup(url_movie, id_number, 1)
+    number_quote = find_tag_dd(soups, 4)
+    return find_number_boucle(analyse_number=number_quote)
 
 
 # Trouve le nombre de page a l'aide du nombre de citation
@@ -36,18 +60,17 @@ def find_number_boucle(analyse_number) -> int:
     return 1
 
 
+# Determine une liste de toutes les citations
 def list_quote(id_number_movie, page_number):
-    html_pages = requests.get(f"{url_movie}{ids_movie[id_number_movie]}-{page_number}")
-    soup = BeautifulSoup(html_pages.text, 'html.parser')
-    quote_links = soup.find_all('a', href=lambda href: href and '/citation-' in href)
+    soups = html_soup(url_movie, ids_movie[id_number_movie], page_number)
+    quote_links = soups.find_all('a', href=lambda href: href and '/citation-' in href)
     for link in quote_links:
         quote_list.append(link.text)
 
 
+# Determine une liste de tout les personnages
 def list_character(id_number_movie, page_number):
-    html_pages = requests.get(f"{url_movie}{ids_movie[id_number_movie]}-{page_number}")
-    soup = BeautifulSoup(html_pages.text, 'html.parser')
-    # character_links = soup.find_all('a', href=lambda href: href and '/perso-' in href)
-    character_links = soup.find_all('footer')
+    soups = html_soup(url_movie, ids_movie[id_number_movie], page_number)
+    character_links = soups.find_all('footer')
     for link in character_links:
         character_list.append(link.text)
